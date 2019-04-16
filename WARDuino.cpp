5872420090c9ee05f54b5a4a7a98c5ab7ff66b6f
Christophe Scholliers <christophe.scholliers@ugent.be>
2019-04-16 17:52:12 +0200

    Arduino primitives for Blink

--- WARDuino.cpp
+++ WARDuino.cpp
@@ -1870,13 +1870,14 @@ int WARDuino::run_module(uint8_t *bytes, int size)
     Options opts;
     initTypes();
     install_primitives();
+
     Module *m = load_module(bytes, size, opts);
     m->sp = -1;
     m->fp = -1;
     m->csp = -1;
     m->sp++;
     //Set the arguments
-    StackValue* sv = &m->stack[m->sp];
+    /*StackValue* sv = &m->stack[m->sp];
     sv->value.uint32 = 0x20; // 32
     sv->value_type = m->functions[1].type->params[0];
     printf("top of stack :: %s \n",value_repr(&m->stack[m->sp]));
@@ -1885,9 +1886,9 @@ int WARDuino::run_module(uint8_t *bytes, int size)
     sv = &m->stack[m->sp];
     sv->value.uint32 = 0x0a; // 10
     sv->value_type = m->functions[1].type->params[1];
-    printf("top of stack :: %s \n",value_repr(&m->stack[m->sp]));
+    printf("top of stack :: %s \n",value_repr(&m->stack[m->sp]));*/
 
-    this->invoke(m, 1);
+    this->invoke(m, m->import_count);
     printf("RESULT :: %s \n",value_repr(&m->stack[m->sp]));
     return m->function_count;
 }
