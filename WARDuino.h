5a76666fff91330986df7dea5276291588e935a9
Christophe Scholliers <christophe.scholliers@ugent.be>
2019-04-16 11:16:01 +0200

    class file update

--- WARDuino.h
+++ WARDuino.h
@@ -153,6 +153,9 @@ class WARDuino
 {
 	public: 
          int run_module(uint8_t *bytes, int size);
+         Module* load_module(uint8_t *bytes, uint32_t byte_count, Options options);
+         bool invoke(Module *m, uint32_t fidx);
+         uint32_t get_export_fidx(Module *m, char *name);
 };
 
 typedef void (*Primitive)(Module*);
