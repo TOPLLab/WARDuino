2a1fcf2dfe0838d0a6a6addbc6ff0ff96f9a5888
Christophe Scholliers <christophe.scholliers@ugent.be>
2019-04-16 17:52:12 +0200

    Arduino primitives for Blink

--- primitives.cpp
+++ primitives.cpp
@@ -4,34 +4,65 @@
  * 
  * Adding a primitive:
  *  1) Bump up the NUM_PRIMITIVES constant
- *  2) Define <primitive>_str 
- *  3) Define <primitive>_type 
- *  4) Define a function void primitive(Module* m)
- *  5) Extend the install_primitives function 
+ *  2) Define <primitive>_type 
+ *  3) Define a function void primitive(Module* m)
+ *  4) Extend the install_primitives function 
  * 
  */
 #include "primitives.h"
+
+extern "C" {
+  #include "util.h"
+}
+
+//#define ARDUINO
+
+#ifdef ARDUINO
+#include "Arduino.h"
+#endif
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 #define NUM_PRIMITIVES 2
+#ifdef ARDUINO
+  #define NUM_PRIMITIVES_ARDUINO 4
+#else
+  #define NUM_PRIMITIVES_ARDUINO 0
+#endif 
+
+#define ALL_PRIMITIVES  NUM_PRIMITIVES + NUM_PRIMITIVES_ARDUINO
+
 // Global index for installing primitives
 int prim_index = 0;
-// Constant strings for the primitive names
-typedef const char *str_type;
+
+
+
 
 /*
     Private macros to install a primitive
 */
 #define install_primitive(prim_name)        \
-  {                                         \
-    PrimitiveEntry* p = &primitives[prim_index++]; \
-    p->name = #prim_name;             \
-    p->f = &prim_name;                 \
-  }
+  {    \
+    printf("installing primitive number: %d  of %d with name: %s\n",prim_index,ALL_PRIMITIVES,#prim_name);\
+    if( prim_index < ALL_PRIMITIVES ) {                                      \
+      PrimitiveEntry* p = &primitives[prim_index++]; \
+      p->name = #prim_name;             \
+      p->f = &prim_name;                 \
+    } else {\
+        FATAL("pim_index out of bounds");\
+    }\
+  }\
 
-#define get_arg(m,arg) m->stack[m->sp+arg].value
+
+#define def_prim(function_name,type)  \
+  Type function_name ## _type = type; \
+  void function_name(Module* m)   
+
+
+#define pop_args(n) m->sp -= n
+#define get_arg(m,arg) m->stack[m->sp-arg].value
 #define arg0 get_arg(m,0)
 #define arg1 get_arg(m,1)
 #define arg2 get_arg(m,2)
@@ -40,53 +71,121 @@ typedef const char *str_type;
 
 
 // The primitive table
-PrimitiveEntry primitives[NUM_PRIMITIVES];
+PrimitiveEntry primitives[ALL_PRIMITIVES];
 
 //------------------------------------------------------
 // Primitive Blink
 //------------------------------------------------------
-Type blink_type;
+Type nullType;
 
-void blink(Module *m)
+def_prim(blink, nullType) 
 {
   size_t cnt = arg0.uint32;
   for (size_t i = 1; i < cnt; i++)
   {
     printf("BLINK %u/%u!\n", i, cnt);
   }
-  m->sp--;
+  pop_args(1);
 }
 
 //------------------------------------------------------
 // Primitive Flash
 //------------------------------------------------------
-Type flash_type;
 
-void flash(Module *m)
+def_prim(flash, nullType)
 {
   size_t cnt = arg0.uint32;
   for (size_t i = cnt; i > 0; i--)
   {
     printf("FLASH %u/%u!\n", i, cnt);
   }
-  m->sp--;
+  pop_args(1);
 }
+
+
+//------------------------------------------------------
+// Arduino Specific Functions
+//------------------------------------------------------
+
+#ifdef ARDUINO
+
+def_prim(chip_pin_mode,nullType) {   
+  printf("chip_pin_mode \n");
+
+  printf("agr0 %u arg1 %u \n",arg0.uint32,arg1.uint32);
+
+  uint8_t pin  = arg1.uint32;
+  uint8_t mode = arg0.uint32;
+
+  printf("agr0 %u arg1 %u \n",pin,mode);
+
+
+  pinMode(pin, mode);
+  printf("DONE pinMode\n");
+
+  pop_args(2);
+    printf("DONE chip_pin_mode\n");
+
+}
+
+def_prim(chip_digital_write, nullType) {
+  printf("chip_digital_write \n");
+  uint8_t pin = arg1.uint32;
+  uint8_t val = arg0.uint32;
+  digitalWrite(pin, val);
+  pop_args(2);
+}
+
+def_prim(chip_delay, nullType){ 
+  printf("chip_delay \n");
+  delay(arg0.uint32);
+  pop_args(0);
+}
+
+def_prim(chip_digital_read, nullType) {
+  uint8_t pin = arg0.uint32;
+  pop_args(1);
+  //pushInt32(digitalRead(pin));
+}
+
+
+
+#endif
+
+/* 
+int analogRead(uint8_t pin)
+void analogReference(uint8_t mode)
+void analogWrite(uint8_t pin, int val)
+void analogWriteFreq(uint32_t freq)
+void analogWriteRange(uint32_t range)
+*/
+
 
 //------------------------------------------------------
 // Installing all the primitives
 //------------------------------------------------------
 void install_primitives(void)
 {
+  printf("INSTALLING PRIMITIVES\n");
   install_primitive(blink);
   install_primitive(flash);
+
+  #ifdef ARDUINO
+      printf("INSTALLING ARDUINO\n");
+      install_primitive(chip_pin_mode);
+      install_primitive(chip_digital_write);
+      install_primitive(chip_digital_read);
+      install_primitive(chip_delay);
+  #endif 
 }
+
 
 //------------------------------------------------------
 // resolving the primitives
 //------------------------------------------------------
 bool resolve_primitive(char *symbol, Primitive *val)
 {
-  for (size_t i = 0; i < NUM_PRIMITIVES; i++)
+  for (size_t i = 0; i < ALL_PRIMITIVES; i++)
   {
     if (!strcmp(symbol, primitives[i].name))
     {
@@ -94,5 +193,6 @@ bool resolve_primitive(char *symbol, Primitive *val)
       return true;
     }
   }
+  FATAL( "Could not find primitive %s \n" , symbol);
   return false;
 }
--- wa_sources/hello_world.wast
+++ wa_sources/hello_world.wast
@@ -1,21 +1,49 @@
-(module
- (import "esp8266" "blink" (func $blink (type $1))) 
- (type $0 (func (param i32) (param i32) (result i32)))
- (type $1 (func (param i32) (result i32)))
+(module 
+ (; Arduino imports ;)
 
- (memory $0 256 256)
+ (import "esp8266" "chip_pin_mode"      (func $pin_mode      (type $1))) 
+ (import "esp8266" "chip_digital_write" (func $digital_write (type $2))) 
+ (import "esp8266" "chip_delay"         (func $delay         (type $3))) 
+ (import "esp8266" "blink"              (func $blink_a       (type $5))) 
+
+ (; Type declarations ;)
+ (type $1 (func (param i32) (param i32) (result)))
+ (type $2 (func (param i32) (param i32) (result)))
+ (type $3 (func (param i32)             (result)))
+ (type $4 (func (param)                 (result)))
+ (type $5 (func (param i32)             (result)))
+
+ (; Define one function ;)
+ (export "blink_arduino" (func $blink_arduino))
+
+ (; The blink function ;)
+ (func $blink_arduino (type $4) 
+   (;  LED    ;)
+    (i32.const 16)
+    (; OUTPUT ;)
+    (i32.const 1) 
+    (call $pin_mode)
 
 
- (export "add" (func $add))
+  (loop
+    (;  LED    ;)
+    (i32.const 16)
+    (; HIGH INVERTED ;)
+    (i32.const 0)
+    (call $digital_write)
 
- (func $add (; 0 ;) (type $0) (param $x i32) (param $y i32) (result i32)
-  (get_local $x)
-  (call $blink)
-  (get_local $y)
-  (call $blink)
-  (get_local $x)
-  (get_local $y)
-  (i32.add)
-  (return))
+    (;  Delay time   ;)
+    (i32.const 100) 
+    (call $delay)
 
+    (;  LED    ;)
+    (i32.const 16)
+    (; LOW INVERTED ;)
+    (i32.const 1)
+    (call $digital_write)
+
+    (br 0))
+
+  (call $blink_a (i32.const 5)))
+ 
 )
