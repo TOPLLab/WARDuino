78ef63d6dd3779326ad657427159914cfe50777a
Christophe Scholliers <christophe.scholliers@ugent.be>
2019-04-16 17:52:32 +0200

    Fixed the allocate 0 bytes

--- mem.c
+++ mem.c
@@ -5,21 +5,31 @@
 #include <string.h>
 
 // Assert calloc
-void *acalloc(size_t nmemb, size_t size,  char *name) {
-    debug("IN Acalloc  count: %d, size: %d for %s \n",nmemb, size,name);
-    void *res = calloc(nmemb, size);
-    debug("Done ... Acalloc\n");
-    if (res == NULL) {
-
-        FATAL("Could not allocate %d bytes for %s \n", (int)(nmemb * size), name);
+void *acalloc(size_t nmemb, size_t size, char *name)
+{
+    if ((int)(nmemb * size) == 0)
+    {
+        return NULL;
     }
-    return res;
+    else
+    {
+        debug("IN Acalloc  count: %d, size: %d for %s \n", nmemb, size, name);
+        void *res = calloc(nmemb, size);
+        debug("Done ... Acalloc\n");
+        if (res == NULL)
+        {
+            FATAL("Could not allocate %d bytes for %s \n", (int)(nmemb * size), name);
+        }
+        return res;
+    }
 }
 
 // Assert realloc/calloc
-void *arecalloc(void *ptr, size_t old_nmemb, size_t nmemb, size_t size,  char *name) {
-    size_t* res = (size_t*)calloc(nmemb, size);
-    if (res == NULL) {
+void *arecalloc(void *ptr, size_t old_nmemb, size_t nmemb, size_t size, char *name)
+{
+    size_t *res = (size_t *)calloc(nmemb, size);
+    if (res == NULL)
+    {
         FATAL("Could not allocate %d bytes for %s", (int)(nmemb * size), name);
     }
     memmove(res, ptr, old_nmemb * size);
