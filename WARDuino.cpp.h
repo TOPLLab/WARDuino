65431ed7a601339685edd8a52b46ba7498ccd6dd
beardhatcode <mail.garonn@gmail.com>
2019-04-23 19:02:59 +0200

    Make git_export_fidx use a fix: add const string

--- WARDuino.cpp.h
+++ WARDuino.cpp.h
@@ -7,5 +7,5 @@ class WARDuino
          int run_module(uint8_t *bytes, int size);
          Module* load_module(uint8_t *bytes, uint32_t byte_count, Options options);
          bool invoke(Module *m, uint32_t fidx);
-         uint32_t get_export_fidx(Module *m, char *name);
+         uint32_t get_export_fidx(Module *m, const char *name);
 };
