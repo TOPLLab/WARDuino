d68fbd289691852d56d0091586712701fbed6860
Christophe Scholliers <christophe.scholliers@ugent.be>
2019-04-23 11:00:21 +0200

    benchmarks

---
+++ benchmarks/tak/wast/tak.c
@@ -0,0 +1,18 @@
+#include <emscripten/emscripten.h>
+
+int EMSCRIPTEN_KEEPALIVE tak(int x, int y, int z)
+{
+  if( !(y < x) ) {
+    return z;
+  }
+  else {
+    return tak(tak(x-1,y,z),
+               tak(y-1,z,x),
+               tak(z-1,x,y));
+  }
+
+}
+
+int main() {
+	return tak(18,12,6);
+}
