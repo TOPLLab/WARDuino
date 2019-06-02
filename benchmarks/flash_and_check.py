d75d638a194170b3c06c9060602dcaa0426dd064
Robbert Gurdeep Singh <mail.garonn@gmail.com>
2019-06-02 12:25:50 +0200

    Add: option to first send file + choose port

--- benchmarks/flash_and_check.py
+++ benchmarks/flash_and_check.py
@@ -1,5 +1,12 @@
+"""
+Write a file to the serial device and count the number of seconds
+between the emmission of "START" and "DONE".
+"""
+
 import serial
+import serial.tools.list_ports
 import time
+import sys
 
 
 
@@ -7,7 +14,11 @@ def awaitOutput(serial:serial.Serial, target:str):
     target = target.encode('utf-8')
     curpos = 0
     while True:
-        x = serial.read()[0]
+        x = serial.read()
+        print(x.decode('utf-8'),end="",file=sys.stderr)
+        x=x[0]
+        if x == b'\r'[0]:
+            continue
         if(x == target[curpos]):
             curpos+=1
             if curpos == len(target):
@@ -18,11 +29,28 @@ def awaitOutput(serial:serial.Serial, target:str):
 
 
 if __name__ == "__main__":
-    
-    with serial.Serial('/dev/ttyUSB0', 115200) as serial:
-        awaitOutput(serial,"START")
+
+    ports=[port.device for port in serial.tools.list_ports.comports()]
+    if len(ports) > 1:
+        print("More than one serial port found!")
+        for i in range(len(ports)):
+            print(f"{i}: {ports[i]}")
+        port=ports[int(input(f"Select port [0-{len(ports)-1}]:"))]
+    else:
+        port = ports[0]
+    print(f"using {port}",file=sys.stderr)
+
+
+    with serial.Serial(port, 115200) as serial:
+        if len(sys.argv) == 2:
+            with open(sys.argv[1],"rb") as inputText:
+                serial.write(inputText.read(-1))
+                serial.write(b'\n\n')
+            print("Bytes sent",file=sys.stderr)
+        awaitOutput(serial,"START\n")
         startTime = time.monotonic()
-        awaitOutput(serial,"DONE")
+        print("START found, waiting for DONE",file=sys.stderr)
+        awaitOutput(serial,"DONE\n")
         endTime = time.monotonic()
         print(endTime-startTime)
             
