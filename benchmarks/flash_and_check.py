24d7da907f91d02e6a1c9733f1121af1b58680a1
beardhatcode <mail.garonn@gmail.com>
2019-06-03 13:53:03 +0200

    clean: more error checks

--- benchmarks/flash_and_check.py
+++ benchmarks/flash_and_check.py
@@ -9,18 +9,17 @@ import time
 import sys
 
 
-
-def awaitOutput(serial:serial.Serial, target:str):
+def await_output(s: serial.Serial, target: str):
     target = target.encode('utf-8')
-    curpos = 0
+    curpos: int = 0
     while True:
-        x = serial.read()
-        print(x.decode('utf-8'),end="",file=sys.stderr)
-        x=x[0]
+        x = s.read()
+        print(x.decode('utf-8'), end="", file=sys.stderr)
+        x = x[0]
         if x == b'\r'[0]:
             continue
-        if(x == target[curpos]):
-            curpos+=1
+        if x == target[curpos]:
+            curpos += 1
             if curpos == len(target):
                 curpos = 0
                 return
@@ -29,28 +28,29 @@ def awaitOutput(serial:serial.Serial, target:str):
 
 
 if __name__ == "__main__":
-
-    ports=[port.device for port in serial.tools.list_ports.comports()]
-    if len(ports) > 1:
+    port = None
+    ports = [port.device for port in serial.tools.list_ports.comports()]
+    if not ports:
+        print("Device not found!")
+        exit(1)
+    elif len(ports) > 1:
         print("More than one serial port found!")
         for i in range(len(ports)):
             print(f"{i}: {ports[i]}")
-        port=ports[int(input(f"Select port [0-{len(ports)-1}]:"))]
+        port = ports[int(input(f"Select port [0-{len(ports) - 1}]:"))]
     else:
         port = ports[0]
-    print(f"using {port}",file=sys.stderr)
-
+    print(f"using {port}", file=sys.stderr)
 
     with serial.Serial(port, 115200) as serial:
         if len(sys.argv) == 2:
-            with open(sys.argv[1],"rb") as inputText:
+            with open(sys.argv[1], "rb") as inputText:
                 serial.write(inputText.read(-1))
                 serial.write(b'\n\n')
-            print("Bytes sent",file=sys.stderr)
-        awaitOutput(serial,"START\n")
+            print("Bytes sent", file=sys.stderr)
+        await_output(serial, "START\n")
         startTime = time.monotonic()
-        print("START found, waiting for DONE",file=sys.stderr)
-        awaitOutput(serial,"DONE\n")
+        print("START found, waiting for DONE", file=sys.stderr)
+        await_output(serial, "DONE\n")
         endTime = time.monotonic()
-        print(endTime-startTime)
-            
+        print(endTime - startTime)
