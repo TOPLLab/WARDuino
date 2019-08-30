8ed3acbce2cce0d9cc58348dca2dfdebe118390c
Christophe Scholliers <christophe.scholliers@ugent.be>
2019-08-30 17:36:35 +0200

    Add start of breakout game
    Co-Authored-By: robbert Gurdeep Singh <Robbert.GurdeepSingh@UGent.be>

---
+++ demo/main.template.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include "../../WARDuino.h"
+#include <csignal>
+
+extern "C" {
+// TODO: Stat files, alternative needed for arduino
+#include <sys/stat.h>
+// END
+}
+
+WARDuino wac;
+
+volatile bool handelingInterrupt = false;
+
+void signalHandler(int /* signum */) {
+    if (handelingInterrupt) return;
+
+    printf("CHANGE REQUESTED!");
+    struct stat statbuff{};
+    if (stat("/tmp/change", &statbuff) == 0 && statbuff.st_size > 0) {
+        uint8_t *data = (uint8_t *)malloc(statbuff.st_size * sizeof(uint8_t));
+        FILE *fp = fopen("/tmp/change", "rb");
+        fread(data, statbuff.st_size, 1, fp);
+        fclose(fp);
+        wac.handleInterrupt(statbuff.st_size, data);
+    }
+
+    handelingInterrupt = false;
+}
+
+/**
+ * Run code, ececute interrups in /tmp/change if a USR1 signal comes
+*/
+int main(int /*argc*/, const char **/*argv*/) {
+    signal(SIGUSR1, signalHandler);
+    wac.run_module(wac.load_module({{src}}, {{src}}_len,{}));
+    return 0;
+}
