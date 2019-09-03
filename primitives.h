609dd3d841990c11c39c9c6238b2910223f92b1f
Robbert Gurdeep Singh <mail.garonn@gmail.com>
2019-09-03 23:28:42 +0200

    Add lookup memory + reindent
    The reindent was by accident

--- primitives.h
+++ primitives.h
@@ -19,7 +19,27 @@ work. If not, see <http://creativecommons.org/licenses/by-nc-sa/4.0/>.
 
 #include "WARDuino.h"
 
+/**
+ * Find a primitive function by looking it up
+ *
+ * @param symbol name of primitive function
+ * @param val    location to store a pointer to the function
+ * @return true if function is found and assigned to val
+ */
 bool resolve_primitive(char *symbol, Primitive *val);
+
+/**
+ * Handle import of memory by keeping a array of 256 pages.
+ * If more imports of memory are needed, this should be replaced by a
+ * nicer function that allows you to specify the different kinds of
+ * memory that mey be needed
+ * 
+ * @param symbol name
+ * @param val    location to store memory to
+ * @return true if memory is found and assigned to val
+ */
+bool resolve_external_memory(char *symbol, Memory** val);
+
 void install_primitives();
 
 #endif
