a954b728a86fa1bcfea943c4856d3210d9740c7b
beardhatcode <Robbert.GurdeepSingh@UGent.be>
2019-09-06 15:06:33 +0200

    Add timer to fetch debugging messages to demo's
    We also added the chip to the name of the template as different chips
    will require a different template (different pins and available headers)

---
+++ demo/main.esp8266.template.ino
@@ -0,0 +1,48 @@
+#include "Arduino.h"
+#include "WARDuino.h"
+#include <Ticker.h>
+WARDuino wac;
+
+#define  DBGTIMEOUT 500000
+
+
+volatile bool handlingInterrupt = false;
+uint8_t buff[100] = {0};
+uint8_t buff_len = 0;
+
+void ICACHE_RAM_ATTR dbgCheck(){
+    if (handlingInterrupt) return;
+    handlingInterrupt = true;
+    interrupts();
+
+    while (Serial.available()) {
+        size_t buff_len = 0;
+        while (Serial.available()) {
+            buff[buff_len++] = (int8_t) Serial.read();
+        }
+        if (buff_len) {
+            wac.handleInterrupt(buff_len, buff);
+        }
+    }
+    handlingInterrupt = false;
+
+    // restart timer
+    timer1_write(DBGTIMEOUT);
+}
+
+void setup()
+{
+    Serial.begin(115200);
+
+    // Set timer
+    timer1_attachInterrupt(dbgCheck);
+    timer1_enable(TIM_DIV16, TIM_EDGE, TIM_SINGLE);
+    timer1_write(DBGTIMEOUT);
+}
+
+void loop() {
+    Module* m = wac.load_module(
+            {{src}}, {{src}}_len,
+            {.disable_memory_bounds = false, .mangle_table_index=false, .dlsym_trim_underscore=true});
+    wac.run_module(m);
+}
