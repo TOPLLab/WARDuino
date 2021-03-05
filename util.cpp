cb9c7ea2b57c9793a14dfec640beeb54c986a55c
Tom <tom.lauwaerts@ugent.be>
2021-03-05 15:52:11 +0100

    Fix negative integers in read_LEB_signed
    Fixes fac-opt core test.

--- util.cpp
+++ util.cpp
@@ -3,6 +3,8 @@
 #include <cmath>
 #include <cstring>
 
+#include <cstdint>
+
 #include "debug.h"
 
 // Little endian base
@@ -27,8 +29,8 @@ uint64_t read_LEB_(uint8_t **pos, uint32_t maxbits, bool sign) {
         }
     }
     if (sign && (shift < maxbits) && (byte & 0x40u)) {
-        // Sign extend
-        result |= -(1u << shift);
+        // Sign extend by highest bits set 1 except last shift bits
+        result |= UINT64_MAX << shift;
     }
     return result;
 }
