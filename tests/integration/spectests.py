e1df19620524fed2ce447fc7eeb17c430e459f56
Tom <tom.lauwaerts@ugent.be>
2021-03-26 12:35:55 +0100

    Add python script to run spec tests

---
+++ tests/integration/spectests.py
@@ -0,0 +1,67 @@
+import os
+import shutil
+import subprocess
+
+TEST_WARDUINO_EXECUTABLE = "../../cmake-build-debug/TestWARDuino"
+
+
+def main(test_directory):
+    if test_directory[-1] != "/":
+        test_directory += "/"
+
+    # Create temporary directory
+    tmp_directory = test_directory + ".tmp/"
+    if not os.path.exists(tmp_directory):
+        os.mkdir(tmp_directory)
+
+    # For each test file in the test directory
+    for filename in os.listdir(test_directory):
+        if filename.endswith(".wast"):
+            print(f"""\n====== TESTING {filename} ======""")
+
+            base_name = tmp_directory + os.path.splitext(filename)[0]
+            modules_file = open(base_name + "_modules.wast", "w")
+            asserts_file = open(base_name + "_asserts.wast", "w")
+
+            file = modules_file
+            module = False
+            for line in open(test_directory + filename, "r"):
+                if line.startswith("(module"):
+                    if module:
+                        modules_file.close()
+                        asserts_file.close()
+
+                        try:
+                            subprocess.run([TEST_WARDUINO_EXECUTABLE, modules_file.name, asserts_file.name, "wat2wasm"])
+                        except:
+                            pass
+
+                        modules_file = open(base_name + "_modules.wast", "w")
+                        asserts_file = open(base_name + "_asserts.wast", "w")
+                        print("\nnew module")
+
+                    file = modules_file
+                    module = True
+                elif line.startswith("(assert"):
+                    file = asserts_file
+
+                if not line.startswith(";;"):
+                    file.write(line)
+
+            modules_file.close()
+            asserts_file.close()
+
+            try:
+                subprocess.run([TEST_WARDUINO_EXECUTABLE, modules_file.name, asserts_file.name, "wat2wasm"])
+            except subprocess.CalledProcessError:
+                pass
+
+    # Remove temporary files
+    try:
+        shutil.rmtree(tmp_directory)
+    except OSError as e:
+        print("Error: %s - %s." % (e.filename, e.strerror))
+
+
+if __name__ == '__main__':
+    main("core/")
