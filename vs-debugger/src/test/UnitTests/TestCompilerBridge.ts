c8df45e0efc911404aa54e72f50cdd43985bb3bb
Tom Lauwaerts <tom.lauwaerts@ugent.be>
2022-03-08 10:36:21 +0100

    Fix Mocha tests + Add to CI

--- vs-debugger/src/test/UnitTests/TestCompilerBridge.ts
+++ vs-debugger/src/test/UnitTests/TestCompilerBridge.ts
@@ -1,39 +1,32 @@
-
 import 'mocha';
-import { WASMCompilerBridge } from  '../../CompilerBridges/WASMCompilerBridge';
-import { expect } from 'chai';
+import {WASMCompilerBridge} from '../../CompilerBridges/WASMCompilerBridge';
+import {expect} from 'chai';
 
-let runPath = process.cwd()
-let toolChainPath      = ""
-let wasmDirectoryPath  = `${runPath}/src/test/UnitTests/TestSource`
+let runPath = process.cwd();
+let wasmDirectoryPath = `${runPath}/src/test/UnitTests/TestSource`;
 
 suite('WARDuinoCompilerBridge Test Suite', () => {
-      
+
     test('TestCompileOK', async () => {
-            let compilerBridge = new WASMCompilerBridge(toolChainPath,`${wasmDirectoryPath}/fac_ok.wast`);
-            let result = await compilerBridge.compile().catch( (reason) => {
+        let compilerBridge = new WASMCompilerBridge(`${wasmDirectoryPath}/fac_ok.wast`);
+        let result = await compilerBridge.compile().catch((reason) => {
                 expect.fail(`Unexpected exception ${reason}`);
-              }
-            );
-            expect(result[0].lineAddress).to.equal('000002e');
-            expect(result[0].lineInfo.line).to.equal(13);
+            }
+        );
+        expect(result[0].lineAddress).to.equal('000002e');
+        expect(result[0].lineInfo.line).to.equal(13);
     });
 
     test('TestCompileBridgeSyntaxError', async () => {
-        let compilerBridge = new WASMCompilerBridge(toolChainPath,`${wasmDirectoryPath}/fac_syntax_error.wast`);
-        let result = await compilerBridge.compile().catch( (reason) => {
-                expect(reason.line).to.equal(1);
-                expect(reason.column).to.equal(2);
+        let compilerBridge = new WASMCompilerBridge(`${wasmDirectoryPath}/fac_syntax_error.wast`);
+        let result = await compilerBridge.compile().catch((reason) => {
+                expect(reason.lineInfo.line).to.equal(1);
+                expect(reason.lineInfo.column).to.equal(2);
                 expect(reason.message).to.contain('error: unexpected token "modul"');
             }
         );
         expect(result).to.be.undefined;
     });
-
-
-    
-
-
 
 
 })
