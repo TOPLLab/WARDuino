ee5fb0d96783264279e91e8056bec0ae1b5a7d8b
Tom Lauwaerts <tom.lauwaerts@ugent.be>
2022-03-10 15:52:27 +0100

    Move CallbackHandler to separate file

--- ESPWASM/main/WARDuino/WARDuino.h
+++ ESPWASM/main/WARDuino/WARDuino.h
@@ -7,10 +7,10 @@
 #include <cstdio>
 #include <map>
 #include <string>
-#include <unordered_map>
 #include <vector>
 
 #include "../Debug/debugger.h"
+#include "CallbackHandler.h"
 
 // Constants
 #define WA_MAGIC 0x6d736100
@@ -175,44 +175,6 @@ typedef struct PrimitiveEntry {
     Primitive f;
     Type t;
 } PrimitiveEntry;
-
-class Event {
-   public:
-    std::string topic;
-    const char *payload;
-
-    Event(std::string topic, const char *payload);
-};
-
-class Callback {
-   private:
-    Module *module;  // reference to module
-   public:
-    std::string topic;
-    uint32_t table_index{};
-
-    explicit Callback(Module *m, std::string id, uint32_t tidx);
-    Callback(const Callback &c);
-
-    void resolve_event(const Event &e);
-};
-
-class CallbackHandler {
-   private:
-    static std::unordered_map<std::string, std::vector<Callback> *> *callbacks;
-    static std::queue<Event> *events;
-
-    CallbackHandler() = default;  // Disallow creation
-
-   public:
-    //    static bool resolving_event;
-
-    static void add_callback(const Callback &c);
-    static void remove_callback(const Callback &c);
-    static void push_event(std::string topic, const unsigned char *payload,
-                           unsigned int length);
-    static bool resolve_event();
-};
 
 class WARDuino {
    private:
