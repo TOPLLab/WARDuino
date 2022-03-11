d0ead1c2b4d7f60abb52df6b76467b66f9d913b3
Christophe <Christophe.Scholliers@UGent.be>
2022-03-11 16:14:36 +0100

    ESP flag

--- ESPWASM/main/Utils/util_arduino.cpp
+++ ESPWASM/main/Utils/util_arduino.cpp
@@ -6,6 +6,18 @@ void reset_wdt() { yield(); }
 
 #else
 
+#ifdef ESP
+#include "esp_task_wdt.h"
+#include "freertos/task.h"
+void reset_wdt() {
+  //taskYIELD();
+		//esp_task_wdt_reset();
+	 esp_task_wdt_reset();
+		//printf("reset wdt  \n");
+}	
+#else
+
 void reset_wdt() {}
+#endif
 
 #endif
