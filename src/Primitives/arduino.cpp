2c8eb64978b02b8fe3c14fee3fc6e35e428617ce
tolauwae <Tom.Lauwaerts@UGent.be>
2022-10-05 14:05:58 +0200

    Add primitives used in benchmarks for Arduino

--- src/Primitives/arduino.cpp
+++ src/Primitives/arduino.cpp
@@ -132,7 +132,7 @@ int resolve_isr(int pin) {
 // Primitives
 
 #define NUM_PRIMITIVES 0
-#define NUM_PRIMITIVES_ARDUINO 38
+#define NUM_PRIMITIVES_ARDUINO 40
 
 #define ALL_PRIMITIVES (NUM_PRIMITIVES + NUM_PRIMITIVES_ARDUINO)
 
@@ -334,6 +334,23 @@ def_prim(micros, NoneToOneU64) {
     pushUInt64(micros());
     return true;
 }
+
+// Benchmarks
+
+unsigned long bench = 0;
+
+def_prim(bench_start, NoneToNoneU32) {
+    bench = micros();
+    return true;
+}
+
+def_prim(bench_finish, NoneToOneU64) {
+    unsigned long _micros = micros();
+    pushUInt64(_micros - bench);
+    return true;
+}
+
+// Printing
 
 def_prim(print_int, oneToNoneU32) {
     uint32_t integer = arg0.uint32;
@@ -940,6 +957,9 @@ void install_primitives() {
     install_primitive(abort);
     install_primitive(millis);
     install_primitive(micros);
+
+    install_primitive(bench_start);
+    install_primitive(bench_finish);
 
     install_primitive(print_int);
     install_primitive(print_string);
