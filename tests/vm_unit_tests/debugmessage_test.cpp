77bb2c227498038ecab596d7b2b400a82e388f56
Carlos Rojas <carlosj.rojas.castillo@gmail.com>
2022-11-28 15:13:12 +0100

    fix clang format

--- tests/vm_unit_tests/debugmessage_test.cpp
+++ tests/vm_unit_tests/debugmessage_test.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <unistd.h>
+
 #include <tuple>
 
 #include "../../src/Debug/debugger.h"
@@ -7,80 +8,80 @@
 #include "gtest/gtest.h"
 
 class DebuggerMessage : public ::testing::Test {
- protected:
-  const int fd;
+   protected:
+    const int fd;
 
-  // For this tests, we use the channel as a one way communication from the
-  // debugger to the testing class
-  FileDescriptorChannel* channel;
-  Debugger* debugger;
+    // For this tests, we use the channel as a one way communication from the
+    // debugger to the testing class
+    FileDescriptorChannel* channel;
+    Debugger* debugger;
 
-  DebuggerMessage() : fd(fileno(tmpfile())) {
-    channel = new FileDescriptorChannel(fd);
-    debugger = new Debugger(channel);
-  }
-
-  ~DebuggerMessage() override {
-    close(fd);
-    delete channel;
-    delete debugger;
-  }
-
-  void SetUp() override {}
-
-  void TearDown() override {}
-
-  std::tuple<ssize_t, char*> readAnswer() {
-    ssize_t valread;
-    char* buffer = (char*) malloc( sizeof(char) * 1024);
-    valread = channel->read(buffer, 1024);
-    if(valread <= 0){
-      free(buffer);
-      valread = 0;
-      buffer = nullptr;
+    DebuggerMessage() : fd(fileno(tmpfile())) {
+        channel = new FileDescriptorChannel(fd);
+        debugger = new Debugger(channel);
     }
-    return {1, buffer};
-  }
+
+    ~DebuggerMessage() override {
+        close(fd);
+        delete channel;
+        delete debugger;
+    }
+
+    void SetUp() override {}
+
+    void TearDown() override {}
+
+    std::tuple<ssize_t, char*> readAnswer() {
+        ssize_t valread;
+        char* buffer = (char*)malloc(sizeof(char) * 1024);
+        valread = channel->read(buffer, 1024);
+        if (valread <= 0) {
+            free(buffer);
+            valread = 0;
+            buffer = nullptr;
+        }
+        return {1, buffer};
+    }
 };
 
 TEST_F(DebuggerMessage, InitiallyEmpty) {
-  EXPECT_EQ(debugger->getDebugMessage(), nullptr)
-      << "Debugger messages queue should start empty" << std::endl;
+    EXPECT_EQ(debugger->getDebugMessage(), nullptr)
+        << "Debugger messages queue should start empty" << std::endl;
 }
 
 TEST_F(DebuggerMessage, ValidMessageIsQueued) {
-  const uint8_t msg[] = {'0', '1', '\n'};  // run interrupt
-  size_t len = 3;
-  debugger->addDebugMessage(len, msg);
-  EXPECT_NE(debugger->getDebugMessage(), nullptr)
-      << "Valid message should be enqueued" << std::endl;
+    const uint8_t msg[] = {'0', '1', '\n'};  // run interrupt
+    size_t len = 3;
+    debugger->addDebugMessage(len, msg);
+    EXPECT_NE(debugger->getDebugMessage(), nullptr)
+        << "Valid message should be enqueued" << std::endl;
 }
 
 TEST_F(DebuggerMessage, InvalidMessagesAreNotQueued) {
-  const char* msg = "INVALID";
-  size_t len = 7;
-  const short int amount_messages = 5;
-  for (auto i = 0; i < amount_messages; i++) {
-    debugger->addDebugMessage(len, reinterpret_cast<const uint8_t*>(msg));
-  }
-  EXPECT_EQ(debugger->getDebugMessage(), nullptr)
-      << "Invalid messages should be ignored" << std::endl;
+    const char* msg = "INVALID";
+    size_t len = 7;
+    const short int amount_messages = 5;
+    for (auto i = 0; i < amount_messages; i++) {
+        debugger->addDebugMessage(len, reinterpret_cast<const uint8_t*>(msg));
+    }
+    EXPECT_EQ(debugger->getDebugMessage(), nullptr)
+        << "Invalid messages should be ignored" << std::endl;
 }
 
 TEST_F(DebuggerMessage, MessagesAreRequestedInOrder) {
-  const int amount_messages = 10;
-  for (auto i = 0; i < amount_messages; i++) {
-    uint8_t c = i + '0';
-    const uint8_t msg[3] = {'0', c, '\n'};
-    debugger->addDebugMessage(3, msg);
-  }
+    const int amount_messages = 10;
+    for (auto i = 0; i < amount_messages; i++) {
+        uint8_t c = i + '0';
+        const uint8_t msg[3] = {'0', c, '\n'};
+        debugger->addDebugMessage(3, msg);
+    }
 
-  for (auto i = 0; i < amount_messages; i++) {
-    uint8_t* msg = debugger->getDebugMessage();
-    ASSERT_NE(msg, nullptr) << "A queued message was expected" << std::endl;
-    ASSERT_EQ(*msg, i) << "Queued message does not match expected message"
-                       << std::endl;
-  }
+    for (auto i = 0; i < amount_messages; i++) {
+        uint8_t* msg = debugger->getDebugMessage();
+        ASSERT_NE(msg, nullptr) << "A queued message was expected" << std::endl;
+        ASSERT_EQ(*msg, i) << "Queued message does not match expected message"
+                           << std::endl;
+    }
 }
 
 // TEST_F(DebuggerMessage, InvalidCommandIsNotHandled) {
@@ -93,10 +94,11 @@ TEST_F(DebuggerMessage, MessagesAreRequestedInOrder) {
 //   debugger->checkDebugMessages(unusedModule, unusedState);
 //   const char* expected_msg = "COULD not parse interrupt data!\n"
 
-//                              channel.write("COULD not parse interrupt data!\n");
+//                              channel.write("COULD not parse interrupt
+//                              data!\n");
 // }
 
 int main(int argc, char** argv) {
-  ::testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
