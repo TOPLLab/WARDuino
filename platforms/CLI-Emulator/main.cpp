fbc840ab90d03a9f17230ed3b1373753ccc5a0da
Carlos Rojas <carlosj.rojas.castillo@gmail.com>
2022-12-23 12:10:20 +0100

    Fix issue #122

--- platforms/CLI-Emulator/main.cpp
+++ platforms/CLI-Emulator/main.cpp
@@ -121,7 +121,7 @@ void *startDebuggerCommunication(void *arg) {
     uint8_t buffer[1024] = {0};
     while (true) {
         while ((valread = duplex->read(buffer, 1024)) != -1) {
-            WARDuino::instance()->handleInterrupt(valread - 1, buffer);
+            WARDuino::instance()->handleInterrupt(valread, buffer);
         }
     }
 }
