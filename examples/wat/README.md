ed6fa8e1639afb432ffb55bfb2a87d3e2f5c1d17
tolauwae <Tom.Lauwaerts@UGent.be>
2023-01-06 18:22:42 +0100

    Update WAT examples

--- examples/wat/README.md
+++ examples/wat/README.md
@@ -1,6 +1,8 @@
-# Blink in WAT
+# Examples in Textual WebAssembly
 
-This example implements the traditional "blinking LED" example for microcontrollers in WebAssembly Text Format (WAT).
+The `main` folder contains example programs written in textual WebAssembly.
+
+## Compiling and Running the Examples
 
 To compile and flash to an ESP32 with the ESP-IDF toolchain simply run the build script:
 
@@ -9,4 +11,18 @@ bash ./build.sh blink
 ```
 
 The script takes the name of a wat file from the main folder as argument (without extension).
+
+## Blink example
+
+The `blink.wat` file implements the traditional "blinking LED" example for microcontrollers in WebAssembly Text Format (WAT).
+
+## Factorial example
+
+The `fac.wat` file implements the traditional wasm example. The program prints `5!` to the serial port in an infinite loop.
+
+This is also an interesting example to run with the commandline interface:
+
+```bash
+wdcli fac.wasm --invoke fac 10
+```
 
--- examples/wat/main/main.cpp
+++ examples/wat/main/main.cpp
@@ -9,7 +9,6 @@
 #include "driver/uart.h"
 #include "esp_err.h"
 #include "esp_task_wdt.h"
-#include "esp_vfs_dev.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "sdkconfig.h"
@@ -22,33 +21,33 @@ extern "C" {
 extern void app_main(void);
 }
 
-WARDuino wac;
+WARDuino* wac = WARDuino::instance();
 Module* m;
 
 void startDebuggerStd(void* pvParameter) {
+    Channel* duplex = new Duplex(stdin, stdout);
+    wac->debugger->setChannel(duplex);
+    duplex->open();
+
     int valread;
     uint8_t buffer[1024] = {0};
-    wac.debugger->socket = fileno(stdout);
     while (true) {
         taskYIELD();
         vTaskDelay(1000 / portTICK_PERIOD_MS);
 
-        while ((valread = read(fileno(stdin), buffer, 1024)) != -1) {
-            write(fileno(stdout), "got a message ... \n", 19);
-            wac.handleInterrupt(valread - 1, buffer);
-            write(fileno(stdout), buffer, valread);
-            fflush(stdout);
+        while ((valread = duplex->read(buffer, 1024)) != -1) {
+            wac->handleInterrupt(valread - 1, buffer);
         }
     }
 }
 
 void app_main(void) {
-    m = wac.load_module(src_wasm, src_wasm_len, {});
-    uint8_t command[] = {'0', '3', '\n'};
-    wac.handleInterrupt(3, command);
+    m = wac->load_module(src_wasm, src_wasm_len, {});
+    //    uint8_t command[] = {'0', '3', '\n'};
+    //    wac->handleInterrupt(3, command);
     xTaskCreate(startDebuggerStd, "Debug Thread", 5000, NULL, 1, NULL);
     printf("START\n\n");
-    wac.run_module(m);
+    wac->run_module(m);
     printf("END\n\n");
-    wac.unload_module(m);
+    wac->unload_module(m);
 }
