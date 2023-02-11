3078d9ec69f0846411e6eb3990a28ff99804bf75
tolauwae <Tom.Lauwaerts@UGent.be>
2023-02-11 09:31:07 +0100

    Fix: print floating points as hexstrings

--- src/Debug/debugger.cpp
+++ src/Debug/debugger.cpp
@@ -329,10 +329,10 @@ void Debugger::printValue(StackValue *v, uint32_t idx, bool end = false) const {
                      v->value.uint64);
             break;
         case F32:
-            snprintf(buff, 255, R"("type":"F32","value":%.7f)", v->value.f32);
+            snprintf(buff, 255, R"("type":"F32","value":%)" PRIx32, v->value.uint32);
             break;
         case F64:
-            snprintf(buff, 255, R"("type":"F64","value":%.7f)", v->value.f64);
+            snprintf(buff, 255, R"("type":"F64","value":%)" PRIx64, v->value.uint64);
             break;
         default:
             snprintf(buff, 255, R"("type":"%02x","value":"%)" PRIx64 "\"",
