91251f0ac743fa2e01a9f7dfba075d1c8ea45f88
carllocos <carlosj.rojascastillo@gmail.com>
2023-04-02 18:45:19 +0200

    clang format

--- tests/vm_unit_tests/shared/wasm_files.cpp
+++ tests/vm_unit_tests/shared/wasm_files.cpp
@@ -1,5 +1,6 @@
 #pragma once
 #include "wasm_files.h"
+
 #include <tuple>
 
 #define MAX_MODULE_SIZE (64 * 1024 * 1024)
@@ -8,24 +9,24 @@ const char* ERR_FILE_SIZE = "file is too large";
 const char* ERR_FILE_MEMORY = "not enough memory for wasm binary";
 const char* ERR_FILE_READ = "could not read file";
 
-std::tuple<uint8_t*, unsigned int, const char*> Load_WASM(const char *file_name) {
-    uint8_t *wasm;
+std::tuple<uint8_t*, unsigned int, const char*> Load_WASM(
+    const char* file_name) {
+    uint8_t* wasm;
     unsigned int file_size;
 
-    FILE *file = fopen(file_name, "rb");
-    if (!file)
-        return {nullptr, 0, ERR_FILE_OPEN};
+    FILE* file = fopen(file_name, "rb");
+    if (!file) return {nullptr, 0, ERR_FILE_OPEN};
 
     fseek(file, 0, SEEK_END);
     file_size = ftell(file);
     fseek(file, 0, SEEK_SET);
 
-    if (file_size > MAX_MODULE_SIZE){
+    if (file_size > MAX_MODULE_SIZE) {
         fclose(file);
         return {nullptr, 0, ERR_FILE_SIZE};
     }
 
-    wasm = (uint8_t *)malloc(file_size);
+    wasm = (uint8_t*)malloc(file_size);
     if (!wasm) {
         fclose(file);
         return {nullptr, 0, ERR_FILE_MEMORY};
@@ -41,5 +42,3 @@ std::tuple<uint8_t*, unsigned int, const char*> Load_WASM(const char *file_name)
 
     return {wasm, file_size, nullptr};
 }
-
-
--- tests/vm_unit_tests/shared/wasm_files.h
+++ tests/vm_unit_tests/shared/wasm_files.h
@@ -1,4 +1,5 @@
 #pragma once
 #include <tuple>
 
-std::tuple<uint8_t*, unsigned int, const char*> Load_WASM(const char *file_name);
+std::tuple<uint8_t *, unsigned int, const char *> Load_WASM(
+    const char *file_name);
