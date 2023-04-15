2ab6118ed10d1ed1e1444f2cb226b78ec16e2329
carllocos <carlosj.rojascastillo@gmail.com>
2023-04-15 12:26:35 +0200

    add deserialisation into a stackvalue

--- src/Utils/state_de_serialiase.h
+++ src/Utils/state_de_serialiase.h
@@ -1,1 +1,13 @@
 #pragma once
+
+#include "../WARDuino.h"
+
+/**
+ * Deserialise a global value according the binary protocol and put results in
+ * dest
+ *
+ * @param dest
+ * @param bytes
+ * @return True if successful
+ */
+bool deserialiseStackValueIntoDest(StackValue *dest, uint8_t *bytes);
--- src/Utils/state_de_serialise.cpp
+++ src/Utils/state_de_serialise.cpp
@@ -1,1 +1,12 @@
 #include "state_de_serialiase.h"
+
+bool deserialiseStackValueIntoDest(StackValue *dest, uint8_t *bytes) {
+    uint8_t valtypes[] = {I32, I64, F32, F64};
+    uint8_t type_index = *bytes++;
+    if (type_index >= sizeof(valtypes)) return false;
+    dest->value.uint64 = 0;  // init whole union to 0
+    size_t qb = type_index == 0 || type_index == 2 ? 4 : 8;
+    dest->value_type = valtypes[type_index];
+    memcpy(&dest->value, bytes, qb);
+    return true;
+}
