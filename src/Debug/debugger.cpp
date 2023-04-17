48d8863ec5b22637a7efc5b030692c7256876c40
carllocos <carlosj.rojascastillo@gmail.com>
2023-04-17 21:11:35 +0200

    use new deserialiseStackValue

--- src/Debug/debugger.cpp
+++ src/Debug/debugger.cpp
@@ -1137,20 +1137,8 @@ bool Debugger::handleUpdateGlobalValue(Module *m, uint8_t *data) {
 
     this->channel->write("Global %u being changed\n", index);
     StackValue *v = &m->globals[index];
-    switch (v->value_type) {
-        case I32:
-            v->value.uint32 = read_LEB_signed(&data, 32);
-            break;
-        case I64:
-            v->value.int64 = read_LEB_signed(&data, 64);
-            break;
-        case F32:
-            memcpy(&v->value.uint32, data, 4);
-            break;
-        case F64:
-            memcpy(&v->value.uint64, data, 8);
-            break;
-    }
+    bool decodeType = false;
+    deserialiseStackValue(data, decodeType, v);
     this->channel->write("Global %u changed to %u\n", index, v->value.uint32);
     return true;
 }
