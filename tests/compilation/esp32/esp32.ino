a9f677767bbcac8a208a0eb4f59ccfa07c45bd13
tolauwae <Tom.Lauwaerts@UGent.be>
2023-06-02 16:07:54 +0200

    Update compilation test

--- tests/compilation/esp32/esp32.ino
+++ tests/compilation/esp32/esp32.ino
@@ -6,7 +6,7 @@
 
 #include "Arduino.h"
 
-unsigned char hello_wasm[] = {
+unsigned char wasm[] = {
     0x00, 0x61, 0x73, 0x6d, 0x01, 0x00, 0x00, 0x00, 0x04, 0x05, 0x01, 0x70,
     0x01, 0x01, 0x01, 0x05, 0x03, 0x01, 0x00, 0x01, 0x06, 0x13, 0x03, 0x7f,
     0x01, 0x41, 0x80, 0x10, 0x0b, 0x7f, 0x00, 0x41, 0x80, 0x10, 0x0b, 0x7f,
@@ -14,7 +14,7 @@ unsigned char hello_wasm[] = {
     0x6f, 0x72, 0x79, 0x02, 0x00, 0x0a, 0x5f, 0x5f, 0x64, 0x61, 0x74, 0x61,
     0x5f, 0x65, 0x6e, 0x64, 0x03, 0x01, 0x0b, 0x5f, 0x5f, 0x68, 0x65, 0x61,
     0x70, 0x5f, 0x62, 0x61, 0x73, 0x65, 0x03, 0x02};
-unsigned int hello_wasm_len = 80;
+unsigned int wasm_len = 80;
 
 WARDuino* wac = WARDuino::instance();
 Module* m;
@@ -43,13 +43,35 @@ void startDebuggerStd(void* pvParameter) {
     }
 }
 
-void setup(void) { Serial.begin(115200); }
+void setup(void) {
+    Serial.begin(115200);
+    //    attachInterrupt(UART_PIN, handleInput, CHANGE);
+
+    Serial.println(ESP.getFreeHeap());
+    Serial.println("Total heap:");
+    Serial.println(ESP.getHeapSize());
+    Serial.println("\nFree heap:");
+    Serial.println(ESP.getFreeHeap());
+    Serial.println("\nTotal PSRAM:");
+    Serial.println(ESP.getPsramSize());
+    Serial.println("\nFree PSRAM: ");
+    Serial.println(ESP.getFreePsram());
+}
 
 void loop() {
     disableCore0WDT();
-    m = wac->load_module(hello_wasm, hello_wasm_len, {});
+    m = wac->load_module(wasm, wasm_len, {});
+
+    printf("LOADED \n\n");
+    uint8_t command[] = {'0', '3', '\n'};
+    wac->handleInterrupt(3, command);
+    xTaskCreate(startDebuggerStd, "Debug Thread", 5000, NULL, 1, NULL);
+    printf("START\n\n");
+
+    Serial.println("\nFree heap:");
+    Serial.println(ESP.getFreeHeap());
 
     wac->run_module(m);
-
+    printf("END\n\n");
     wac->unload_module(m);
 }
