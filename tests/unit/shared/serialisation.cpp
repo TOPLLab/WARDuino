27252959605e6027f72055bcf7c7d905bbfd0f17
Carlos Rojas <carlosj.rojas.castillo@gmail.com>
2023-06-12 14:18:15 +0200

    refactor uint32 and uint8 to hexa string in helper class

--- tests/unit/shared/serialisation.cpp
+++ tests/unit/shared/serialisation.cpp
@@ -1,11 +1,28 @@
 #include "serialisation.h"
 
-#include <iomanip>
-#include <iostream>
-#include <sstream>
+#include "../../../src/Utils/util.h"
 
-std::string Serialiser::uint32ToHexString(uint32_t value) {
-    std::ostringstream oss;
-    oss << std::hex << std::setw(8) << std::setfill('0') << value;
-    return oss.str();
+uint8_t* Serialiser::encodeUInt32(uint32_t value, uint8_t* dest) {
+    uint8_t* buff = dest == nullptr ? (uint8_t*)malloc(4) : dest;
+    buff[0] = static_cast<uint8_t>((value >> 24) & 0xFF);
+    buff[1] = static_cast<uint8_t>((value >> 16) & 0xFF);
+    buff[2] = static_cast<uint8_t>((value >> 8) & 0xFF);
+    buff[3] = static_cast<uint8_t>(value & 0xFF);
+    return buff;
+}
+
+void Serialiser::uint32ToHexString(uint32_t value, std::string& dest) {
+    uint8_t buff[4] = {'\0'};
+    Serialiser::encodeUInt32(value, buff);
+    char hexa[9] = {'\0'};
+    chars_as_hexa((unsigned char*)hexa, buff, 4);
+    hexa[8] = '\0';
+    dest = hexa;
+}
+
+void Serialiser::uint8ToHexString(uint8_t value, std::string& dest) {
+    char hexa[3] = {'\0'};
+    chars_as_hexa((unsigned char*)hexa, &value, 1);
+    hexa[2] = '\0';
+    dest = hexa;
 }
