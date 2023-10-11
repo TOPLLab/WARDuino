2182d428728287102e4d9702039ae6289c297956
tolauwae <Tom.Lauwaerts@UGent.be>
2023-10-11 15:29:32 +0200

    Fix debug thread priority

--- platforms/ESP-IDF/main.cpp
+++ platforms/ESP-IDF/main.cpp
@@ -9,7 +9,6 @@
 #include "driver/uart.h"
 #include "esp_err.h"
 #include "esp_task_wdt.h"
-//#include "esp_vfs_dev.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "sdkconfig.h"
@@ -48,7 +47,7 @@ void app_main(void) {
     m = wac->load_module(wasm, wasm_len, {});
     //    uint8_t command[] = {'0', '3', '\n'};
     //    wac->handleInterrupt(3, command);
-    xTaskCreate(startDebuggerStd, "Debug Thread", 5000, NULL, 1, NULL);
+    xTaskCreate(startDebuggerStd, "Debug Thread", 5000, NULL, 10 /**tskIDLE_PRIORITY*/, NULL);
     printf("START\n\n");
     wac->run_module(m);
     printf("END\n\n");
