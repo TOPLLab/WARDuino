3d292238fcf346afa8569da56426370b1173caf1
MaartenS11 <maarten.steevens@gmail.com>
2023-10-15 14:39:53 +0200

    Use digitalPinToBitMask in an attempt to make the code more microcontroller agnostic

--- src/IO/io-arduino.cpp
+++ src/IO/io-arduino.cpp
@@ -7,17 +7,17 @@
 std::vector<PinState*> get_io_state() {
     std::vector<PinState*> ioState;
     for (int i = 0; i < NUM_DIGITAL_PINS; i++) {
+        uint32_t bit_mask = digitalPinToBitMask(i);
         auto *state = new PinState();
         ioState.push_back(state);
         state->pin = i;
         uint8_t port = digitalPinToPort(i);
-        uint8_t output = bitRead(*portModeRegister(port), i % 32); // DDR
-        if (output) {
+        if (*portModeRegister(port) & bit_mask) { // DDR
             state->output = true;
-            state->value = bitRead(*portOutputRegister(port), i % 32);
+            state->value = (*portOutputRegister(port) & bit_mask) > 0;
         } else {
             state->output = false;
-            state->value = bitRead(*portInputRegister(port), i % 32);
+            state->value = (*portInputRegister(port) & bit_mask) > 0;
         }
     }
     return ioState;
