55589775b06dff198da36e86b518d94db5824c5d
MaartenS11 <maarten.steevens@gmail.com>
2023-11-18 23:46:37 +0100

    Make Interpreter functions not virtual when compiling for arduino
    On arduino we only have one interpreter so there is no need for virtual.

--- src/Interpreter/interpreter.h
+++ src/Interpreter/interpreter.h
@@ -1,6 +1,12 @@
 #pragma once
 #include "../WARDuino.h"
 
+#ifdef ARDUINO
+#define VIRTUAL
+#else
+#define VIRTUAL virtual
+#endif
+
 class Interpreter {
    public:
     /**
@@ -9,7 +15,7 @@ class Interpreter {
      * @param block
      * @param sp stack pointer to return to
      */
-    virtual void push_block(Module *m, Block *block, int sp);
+    VIRTUAL void push_block(Module *m, Block *block, int sp);
 
     /**
      * Restore the top frame from the call stack of a module.
@@ -19,7 +25,7 @@ class Interpreter {
      * @param m module
      * @return block that was on top of the stack (the one that we just returned to)
      */
-    virtual Block *pop_block(Module *m);
+    VIRTUAL Block *pop_block(Module *m);
 
     /**
      * Setup a function
@@ -30,7 +36,7 @@ class Interpreter {
      * @param m module to work on
      * @param fidx function id (index in m->functions)
      */
-    virtual void setup_call(Module *m, uint32_t fidx);
+    VIRTUAL void setup_call(Module *m, uint32_t fidx);
 
     /**
      * Start interpretation of the module that has a call set up for it.
@@ -42,44 +48,44 @@ class Interpreter {
     bool interpret(Module *m, bool waiting = false);
 
    protected:
-    virtual void load(Module *m, uint32_t offset, uint32_t addr, int size, uint8_t value_type, bool sign_extend);
-    virtual void store(Module *m, uint32_t offset, uint32_t addr, int value_sp, int size);
+    VIRTUAL void load(Module *m, uint32_t offset, uint32_t addr, int size, uint8_t value_type, bool sign_extend);
+    VIRTUAL void store(Module *m, uint32_t offset, uint32_t addr, int value_sp, int size);
 
-    virtual bool i_instr_block(Module *m, uint8_t *block_ptr);
-    virtual bool i_instr_loop(Module *m, uint8_t *block_ptr);
-    virtual bool i_instr_if(Module *m, uint8_t *block_ptr);
-    virtual bool i_instr_else(Module *m);
-    virtual bool i_instr_end(Module *m, bool *prog_done);
-    virtual bool i_instr_br(Module *m);
-    virtual bool i_instr_br_if(Module *m);
-    virtual bool i_instr_br_table(Module *m);
-    virtual bool i_instr_return(Module *m);
-    virtual bool i_instr_call(Module *m);
-    virtual bool i_instr_call_indirect(Module *m);
-    virtual bool i_instr_drop(Module *m);
-    virtual bool i_instr_select(Module *m);
-    virtual bool i_instr_get_local(Module *m);
-    virtual bool i_instr_set_local(Module *m);
-    virtual bool i_instr_tee_local(Module *m);
-    virtual bool i_instr_get_global(Module *m);
-    virtual bool i_instr_set_global(Module *m);
-    virtual bool i_instr_current_memory(Module *m);
-    virtual bool i_instr_grow_memory(Module *m);
-    virtual bool i_instr_mem_load(Module *m, uint8_t opcode);
-    virtual bool i_instr_mem_store(Module *m, uint8_t opcode);
-    virtual bool i_instr_const(Module *m, uint8_t opcode);
-    virtual bool i_instr_unary_u32(Module *m, uint8_t opcode);
-    virtual bool i_instr_math_u32(Module *m, uint8_t opcode);
-    virtual bool i_instr_math_u64(Module *m, uint8_t opcode);
-    virtual bool i_instr_math_f32(Module *m, uint8_t opcode);
-    virtual bool i_instr_math_f64(Module *m, uint8_t opcode);
-    virtual bool i_instr_unary_i32(Module *m, uint8_t opcode);
-    virtual bool i_instr_unary_i64(Module *m, uint8_t opcode);
-    virtual bool i_instr_unary_floating(Module *m, uint8_t opcode);
-    virtual bool i_instr_binary_i32(Module *m, uint8_t opcode);
-    virtual bool i_instr_binary_i64(Module *m, uint8_t opcode);
-    virtual bool i_instr_binary_f32(Module *m, uint8_t opcode);
-    virtual bool i_instr_binary_f64(Module *m, uint8_t opcode);
-    virtual bool i_instr_conversion(Module *m, uint8_t opcode);
-    virtual bool i_instr_callback(Module *m, uint8_t opcode);
+    VIRTUAL bool i_instr_block(Module *m, uint8_t *block_ptr);
+    VIRTUAL bool i_instr_loop(Module *m, uint8_t *block_ptr);
+    VIRTUAL bool i_instr_if(Module *m, uint8_t *block_ptr);
+    VIRTUAL bool i_instr_else(Module *m);
+    VIRTUAL bool i_instr_end(Module *m, bool *prog_done);
+    VIRTUAL bool i_instr_br(Module *m);
+    VIRTUAL bool i_instr_br_if(Module *m);
+    VIRTUAL bool i_instr_br_table(Module *m);
+    VIRTUAL bool i_instr_return(Module *m);
+    VIRTUAL bool i_instr_call(Module *m);
+    VIRTUAL bool i_instr_call_indirect(Module *m);
+    VIRTUAL bool i_instr_drop(Module *m);
+    VIRTUAL bool i_instr_select(Module *m);
+    VIRTUAL bool i_instr_get_local(Module *m);
+    VIRTUAL bool i_instr_set_local(Module *m);
+    VIRTUAL bool i_instr_tee_local(Module *m);
+    VIRTUAL bool i_instr_get_global(Module *m);
+    VIRTUAL bool i_instr_set_global(Module *m);
+    VIRTUAL bool i_instr_current_memory(Module *m);
+    VIRTUAL bool i_instr_grow_memory(Module *m);
+    VIRTUAL bool i_instr_mem_load(Module *m, uint8_t opcode);
+    VIRTUAL bool i_instr_mem_store(Module *m, uint8_t opcode);
+    VIRTUAL bool i_instr_const(Module *m, uint8_t opcode);
+    VIRTUAL bool i_instr_unary_u32(Module *m, uint8_t opcode);
+    VIRTUAL bool i_instr_math_u32(Module *m, uint8_t opcode);
+    VIRTUAL bool i_instr_math_u64(Module *m, uint8_t opcode);
+    VIRTUAL bool i_instr_math_f32(Module *m, uint8_t opcode);
+    VIRTUAL bool i_instr_math_f64(Module *m, uint8_t opcode);
+    VIRTUAL bool i_instr_unary_i32(Module *m, uint8_t opcode);
+    VIRTUAL bool i_instr_unary_i64(Module *m, uint8_t opcode);
+    VIRTUAL bool i_instr_unary_floating(Module *m, uint8_t opcode);
+    VIRTUAL bool i_instr_binary_i32(Module *m, uint8_t opcode);
+    VIRTUAL bool i_instr_binary_i64(Module *m, uint8_t opcode);
+    VIRTUAL bool i_instr_binary_f32(Module *m, uint8_t opcode);
+    VIRTUAL bool i_instr_binary_f64(Module *m, uint8_t opcode);
+    VIRTUAL bool i_instr_conversion(Module *m, uint8_t opcode);
+    VIRTUAL bool i_instr_callback(Module *m, uint8_t opcode);
 };
