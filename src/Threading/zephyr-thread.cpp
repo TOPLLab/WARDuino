9da472fa2051e9812379ad3c6889291d6cc59215
MaartenS11 <maarten.steevens@gmail.com>
2024-04-07 22:15:26 +0200

    Fixed warduino::condition_variable zephyr implementation

--- src/Threading/zephyr-thread.cpp
+++ src/Threading/zephyr-thread.cpp
@@ -6,6 +6,6 @@
 void warduino::condition_variable::notify_one() { k_condvar_signal(&c); }
 
 void warduino::condition_variable::wait(warduino::unique_lock &l) {
-    k_condvar_wait(&c, &l.m.m, K_FOREVER);
+    k_condvar_wait(&c, &l.m->m, K_FOREVER);
 }
 #endif
