24fc92f5881f73a1fbf69e0ebfffba1322e4f8c4
MaartenS11 <maarten.steevens@gmail.com>
2024-04-11 22:44:23 +0200

    Add back override interrupts
    But now they are better as they work at the VM level and are not just a little bit of code inside a primitive. They also override primitive + argument combinations instead of just overriding a pin which only works for analog and digital read primitives.

--- src/Debug/debugger.h
+++ src/Debug/debugger.h
@@ -90,10 +90,13 @@ enum InterruptTypes {
     interruptDUMPCallbackmapping = 0x74,
     interruptRecvCallbackmapping = 0x75,
 
+    // Primitive overrides
+    interruptSetOverridePinValue = 0x80,
+    interruptUnsetOverridePinValue = 0x81,
+
     // Operations
     interruptStore = 0xa0,
     interruptStored = 0xa1,
-
 };
 
 class Debugger {
@@ -116,6 +119,8 @@ class Debugger {
     warduino::mutex *supervisor_mutex;
 
     bool asyncSnapshots;
+
+    std::unordered_map<uint32_t, std::unordered_map<uint32_t, uint32_t>> overrides;
 
     // Private methods
 
@@ -270,4 +275,11 @@ class Debugger {
     void notifyPushedEvent() const;
 
     bool handlePushedEvent(char *bytes) const;
+
+    // Concolic Multiverse Debugging
+    inline bool isMocked(uint32_t fidx, uint32_t argument) { return overrides.count(fidx) > 0 && overrides[fidx].count(argument) > 0; }
+    inline uint32_t getMockedValue(uint32_t fidx, uint32_t argument) { return overrides[fidx][argument]; }
+
+    void addOverride(Module *m, uint8_t *interruptData);
+    void removeOverride(Module *m, uint8_t *interruptData);
 };
