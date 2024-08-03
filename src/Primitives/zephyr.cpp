01ee63e43a5712a9889e22d951ff21215c179765
MaartenS11 <maarten.steevens@gmail.com>
2024-08-03 15:12:43 +0200

    Only run the reverse operation for the primitive that was just executed and not for all primitives
    This particularly important for motor primitives that for example drive two motors at the same time vs a primitive that drives one motor at a time, executing the wrong reverse operation will result in the correct angles but the wrong position.

--- src/Primitives/zephyr.cpp
+++ src/Primitives/zephyr.cpp
@@ -944,9 +944,21 @@ bool resolve_external_memory(char *symbol, Memory **val) {
 //------------------------------------------------------
 void restore_external_state(Module *m,
                             std::vector<IOStateElement> external_state) {
-    for (auto &primitive : primitives) {
-        if (primitive.f_reverse) {
-            primitive.f_reverse(m, external_state);
+    uint8_t opcode = *m->pc_ptr;
+    // TODO: Maybe primitives can also be called using the other call operators, maybe there should just be a function that checks if a certain function is being called?
+    if (opcode == 0x10) { // call opcode
+        uint8_t *pc_copy = m->pc_ptr + 1;
+        uint32_t fidx = read_LEB_32(&pc_copy);
+        if (fidx < m->import_count) {
+            for (auto &primitive : primitives) {
+                if (!strcmp(primitive.name, m->functions[fidx].import_field)) {
+                    if (primitive.f_reverse) {
+                        printf("Reversing action for primitive %s\n", primitive.name);
+                        primitive.f_reverse(m, external_state);
+                    }
+                    return;
+                }
+            }
         }
     }
 }
