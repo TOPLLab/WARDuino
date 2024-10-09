19d431efe4d4a17cdeec93f85bc9399ebbf7da11
MaartenS11 <maarten.steevens@gmail.com>
2024-10-09 19:59:29 +0200

    Use static_cast instead of c style cast

--- src/Memory/mem.cpp
+++ src/Memory/mem.cpp
@@ -11,7 +11,7 @@
 
 // Assert calloc
 void *acalloc(size_t nmemb, size_t size, const char *name, [[maybe_unused]] bool psram) {
-    if ((int)(nmemb * size) == 0) {
+    if (static_cast<int>(nmemb * size) == 0) {
         return nullptr;
     } else {
         debug("IN Acalloc count: %zu, size: %zu for %s \n", nmemb, size, name);
@@ -28,7 +28,7 @@ void *acalloc(size_t nmemb, size_t size, const char *name, [[maybe_unused]] bool
         debug("Done ... Acalloc\n");
         if (res == nullptr) {
             debug("FAILED ... Acalloc\n");
-            FATAL("Could not allocate %d bytes for %s \n", (int)(nmemb * size),
+            FATAL("Could not allocate %d bytes for %s \n", static_cast<int>(nmemb * size),
                   name);
         }
         debug("NOT FAILED ... Acalloc\n");
@@ -47,10 +47,10 @@ void *arecalloc(void *ptr, size_t old_nmemb, size_t nmemb, size_t size,
         res = (size_t *)calloc(nmemb, size);
     }
 #else
-    auto *res = (size_t *)calloc(nmemb, size);
+    auto *res = static_cast<size_t *>(calloc(nmemb, size));
 #endif
     if (res == nullptr) {
-        FATAL("Could not allocate %d bytes for %s", (int)(nmemb * size), name);
+        FATAL("Could not allocate %d bytes for %s", static_cast<int>(nmemb * size), name);
     }
     memset(res, 0, nmemb * size);  // initialize memory with 0
     memmove(res, ptr, old_nmemb * size);
