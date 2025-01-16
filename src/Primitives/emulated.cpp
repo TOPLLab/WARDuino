380b19df9a18e8addebde680fd13f27d0fd67644
MaartenS11 <maarten.steevens@gmail.com>
2025-01-16 17:17:41 +0100

    Fix pim_index typo

--- src/Primitives/emulated.cpp
+++ src/Primitives/emulated.cpp
@@ -50,7 +50,7 @@ double sensor_emu = 0;
             p->f_reverse = nullptr;                                        \
             p->f_serialize_state = nullptr;                                \
         } else {                                                           \
-            FATAL("pim_index out of bounds");                              \
+            FATAL("prim_index out of bounds");                              \
         }                                                                  \
     }
 
