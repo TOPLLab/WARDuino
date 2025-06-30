7c2516d3b509d9bfb61ec409315ff8cd5bcd1e8c
tolauwae <tolauwae@users.noreply.github.com>
2025-06-30 21:23:18 +0000

    Auto deploy from Github Actions

---
+++ assets/guide_plugin_get-started.md.6cf2df7b.lean.js
@@ -0,0 +1,13 @@
+import{d as n}from"./chunks/config.data.ff113af3.js";import{o as a,c as t,C as s,a as o,t as l,b as p,V as e}from"./chunks/framework.34305d4c.js";const c=e("",7),r={class:"language-json"},_=s("button",{title:"Copy Code",class:"copy"},null,-1),i=s("span",{class:"lang"},"json",-1),D={class:"shiki material-theme-palenight"},u=s("span",{class:"line"},[s("span",{style:{color:"#89DDFF"}},"{")],-1),d={class:"line"},y=e("",7),F={style:{color:"#C3E88D"}},h=s("span",{style:{color:"#89DDFF"}},'"',-1),A=s("span",{style:{color:"#89DDFF"}},",",-1),g=e("",1),C=s("span",{class:"line"},[s("span",{style:{color:"#A6ACCD"}},"        "),s("span",{style:{color:"#89DDFF"}},"{")],-1),T=e("",1),f=e("",1),S=e("",1),m=e("",1),b=e("",1),q=e("",1),P=s("span",{class:"line"},[s("span",{style:{color:"#A6ACCD"}},"        "),s("span",{style:{color:"#89DDFF"}},"}")],-1),V=s("span",{class:"line"},[s("span",{style:{color:"#A6ACCD"}},"    "),s("span",{style:{color:"#89DDFF"}},"]")],-1),I=s("span",{class:"line"},[s("span",{style:{color:"#89DDFF"}},"}")],-1),E=e("",5),j=JSON.parse('{"title":"Getting Started","description":"","frontmatter":{},"headers":[],"relativePath":"guide/plugin/get-started.md","filePath":"guide/plugin/get-started.md","lastUpdated":null}'),k={name:"guide/plugin/get-started.md"},x=Object.assign(k,{setup(R){return(B,N)=>(a(),t("div",null,[c,s("div",r,[_,i,s("pre",D,[s("code",null,[u,o(`
+`),s("span",d,[y,s("span",F,l(p(n).plugin.version),1),h,A]),o(`
+`),g,o(`
+`),C,o(`
+`),T,o(`
+`),f,o(`
+`),S,o(`
+`),m,o(`
+`),b,o(`
+`),q,o(`
+`),P,o(`
+`),V,o(`
+`),I])])]),E]))}});export{j as __pageData,x as default};
