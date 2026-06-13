4bc49697b1157d75a758335eee33f3e5064a5d96
Tom Lauwaerts <tom.lauwaerts@vub.be>
2026-06-13 10:24:30 +0200

    Fix spec.util.ts

--- tests/latch/src/util/spec.util.ts
+++ tests/latch/src/util/spec.util.ts
@@ -1,11 +1,13 @@
 import {WASM} from "latch";
 import {readFileSync} from "fs";
+import Value = WASM.Value;
+import WasmInt = WASM.WasmInt;
 
 interface Cursor {
     value: number;
 }
 
-export function parseResult(input: string): WASM.Value | undefined {
+export function parseResult(input: string): WASM.Value<WASM.Type> | undefined {
     let cursor = 0;
     let delta: number = consume(input, cursor, /\(/d);
     if (delta === 0) {
@@ -14,27 +16,32 @@ export function parseResult(input: string): WASM.Value | undefined {
     cursor += delta;
 
     delta = consume(input, cursor, /^[^.)]*/d);
-    const type: WASM.Type = WASM.typing.get(input.slice(cursor, cursor + delta)) ?? WASM.Type.i64;
+    let type: WASM.Type = WASM.typing.get(input.slice(cursor, cursor + delta)) ?? WASM.Integer.i64;
 
     cursor += delta + consume(input, cursor + delta);
 
-    let value;
     delta = consume(input, cursor, /^[^)]*/d);
-    if (type === WASM.Type.f32 || type === WASM.Type.f64) {
-        value = parseHexFloat(input.slice(cursor, cursor + delta));
-    } else {
-        value = parseInteger(input.slice(cursor, cursor + delta));
-    }
-
-    if (value === undefined) {
-        return value;
-    }
-
-    return {type, value};
+    return parseValue(input.slice(cursor, cursor + delta), type);
 }
 
-export function parseArguments(input: string, index: Cursor): WASM.Value[] {
-    const args: WASM.Value[] = [];
+function parseValue(input: string, type: WASM.Type): Value<WASM.Type> | undefined {
+    let value: number | bigint;
+    switch (type) {
+        case WASM.Float.f32:
+        case WASM.Float.f64:
+            return {value: parseHexFloat(input), type};
+        case WASM.Integer.u32:
+        case WASM.Integer.i32:
+        case WASM.Integer.u64:
+        case WASM.Integer.i64:
+            return {value: parseInteger(input, type), type};
+        default:
+            return undefined;
+    }
+}
+
+export function parseArguments(input: string, index: Cursor): WASM.Value<WASM.Type>[] {
+    const args: WASM.Value<WASM.Type>[] = [];
 
     let cursor: number = consume(input, 0, /invoke "[^"]+"/d);
     while (cursor < input.length) {
@@ -45,19 +52,14 @@ export function parseArguments(input: string, index: Cursor): WASM.Value[] {
         cursor += delta;
 
         delta = consume(input, cursor, /^[^.)]*/d);
-        const type: WASM.Type = WASM.typing.get(input.slice(cursor + delta - 3, cursor + delta)) ?? WASM.Type.i64;
+        const type: WASM.Type = WASM.typing.get(input.slice(cursor + delta - 3, cursor + delta)) ?? WASM.Integer.i64;
 
         cursor += delta + consume(input, cursor + delta, /^[^)]*const /d);
         delta = consume(input, cursor, /^[^)]*/d);
-        let maybe: number | undefined;
-        if (type === WASM.Type.f32 || type === WASM.Type.f64) {
-            maybe = parseHexFloat(input.slice(cursor, cursor + delta));
-        } else {
-            maybe = parseInteger(input.slice(cursor, cursor + delta));
-        }
+        let maybe = parseValue(input.slice(cursor, cursor + delta), type);
 
         if (maybe !== undefined) {
-            args.push({type, value: maybe});
+            args.push(maybe);
         }
 
         cursor += consume(input, cursor, /\)/d);
@@ -131,16 +133,18 @@ function parseHexFloat(input: string): number {
     return mantissa * Math.pow(2, exponent);
 }
 
-function parseInteger(hex: string, bytes: number = 4): number {
+function parseInteger(hex: string, type: WASM.Integer): WasmInt {
+    const bytes = type === WASM.Integer.u32 || type === WASM.Integer.i32 ? 4 : 8;
     if (!hex.includes('0x')) {
-        return parseInt(hex);
+        const n: number = parseInt(hex);
+        return typeof n !== 'bigint' && isNaN(n) ? WasmInt.nan() : typeof n !== 'bigint' && n === Infinity ? WasmInt.infinity() : WasmInt.finite(BigInt(hex));
     }
-    const mask = parseInt('0x80' + '00'.repeat(bytes - 1), 16);
-    let integer = parseInt(hex, 16);
+    const mask = BigInt(parseInt('0x80' + '00'.repeat(bytes - 1), 16));
+    let integer = BigInt(parseInt(hex, 16));
     if (integer >= mask) {
-        integer = integer - mask * 2;
+        integer = integer - mask * 2n;
     }
-    return integer;
+    return WasmInt.finite(integer);
 }
 
 export function find(regex: RegExp, input: string) {
